cd4da6951acf0651da5c01ad2a792221fc3d13c4
Jeyssoon <67297011+Jeyssoon@users.noreply.github.com>
2021-06-15 23:01:42 -0500

    Add files via upload

---
+++ Miprimeraconsola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Miprimeraconsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO!");
+        }
+    }
+}
